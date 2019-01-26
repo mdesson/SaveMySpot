5c89a523bcefa87e15a916b62a61b6a3adbe361e
mdesson <michael.desson@gmail.com>
2019-01-26 13:41:33 -0500

    Added python script to get Concordia Open Data portal JSON file

--- concordiaAPI.py
+++ concordiaAPI.py
@@ -1,2 +1,15 @@
-import requests
+# TODO: Add parameter for fall/winter, etc semesters
 
+import requests
+import json
+
+with open("config.json") as file:
+    config = json.load(file)
+
+user = config["apiUser"]
+key = config["apiKey"]
+url = "https://opendata.concordia.ca/API/v1/course/session/filter/*/*/*"
+
+with open("courses.json", "w") as courses:
+    r = requests.get(url, auth=(user, key))
+    json.dump(r.json(), courses)
