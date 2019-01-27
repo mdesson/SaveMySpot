6f395c129d3c1844c49380cd738485ee5a4953ce
mdesson <michael.desson@gmail.com>
2019-01-26 20:12:55 -0500

    Added comments to SqlQuery.java

--- java/src/SqlQuery.java
+++ java/src/SqlQuery.java
@@ -4,30 +4,36 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 
 class SqlQuery {
-
+	
+	// Database parameters
     private static final String url = "jdbc:mysql://localhost:3306/savemyspotConcordia";
     private static final String user = "root";
     private static final String password = "";
     
+    // Database queries
     private static String query = "SELECT Email, Course_Catalog, Course_Code FROM StudentCourseList WHERE Waitlist='1'";
     private static String querySize = "SELECT COUNT(*) FROM StudentCourseList WHERE Waitlist='1'";
     
+    
+    // Query database for email addresses and course names
     public static String[][] queryDatabase() {
         try {
+        	// Connect to database and create Statement
             Connection con = DriverManager.getConnection(url, user, password);
             System.out.println("Successfully connected to database");
-            
             Statement stmt = stmt = con.createStatement();
             
-            // Get number of results
+            // Get number of results for loop index
             ResultSet resultSize = stmt.executeQuery(query);
             resultSize.last();
             
             // Initialize array to get results
             String[][] output = new String[resultSize.getRow()][2];
             
+            // Get results of query
             ResultSet result = stmt.executeQuery(query);
             
+            // Create array 
             for (int i=0; i<output.length; i++) {
             	result.next();
             	output[i][0] = result.getString("Email");
@@ -38,9 +44,9 @@ class SqlQuery {
                         
         } catch (Exception e) {
             e.printStackTrace();
-            String[][] output = {{"Error"}};
         }
         
+        // Return 2D array for unhandled runtime errors
         return new String[][] {{"Error"}};
         
     }
